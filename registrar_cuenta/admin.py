320ecf6e31b866843774f277dba317af83ecd9cb
GaboDios <diazpayne.gabriel@ciencias.unam.mx>
2026-05-05 11:34:15 -0600

    Agrega admin para modelo Usuario

--- registrar_cuenta/admin.py
+++ registrar_cuenta/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuario
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre_usuario', 'contrasena', 'tipo_usuario')
