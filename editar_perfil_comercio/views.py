8d5d85f12aac11c6a1f407ea027a796e5ce3582a
anasofiahdzz <anasofiahdzz@ciencias.unam.mx>
2026-05-09 14:34:34 -0600

    editar_perfil_restaurante, ya guarda los datos y esta correcto el flujo alternativo

--- editar_perfil_comercio/views.py
+++ editar_perfil_comercio/views.py
@@ -13,26 +13,40 @@ def ver_perfil(request):
 def editar_perfil(request):
     #de muestra el restaurante a ser editado
     restaurante_actual = Restaurante.objects.first()
+    
     if request.method == 'POST':
         # loq ue el usuario escribe en e formulario
         nuevo_nombre = request.POST.get('nombre')
         nueva_direccion = request.POST.get('direccion')
         nuevo_telefono = request.POST.get('telefono')
         nueva_contrasena = request.POST.get('contrasena')
+        
+        #para que cuando haya errores no se borren los datos
+        contexto = {'restaurante': restaurante_actual}
 
-
-        # FLUJO EXCEPCIONAL 2: falla del servidor
+        # FLUJO EXCEPCIONAL: falla del servidor
         if nuevo_nombre == 'Falla': 
             messages.error(request, "Error: Falla del servidor. Intente más tarde.")
-            return render(request, 'editar_comercio.html')
+            return render(request, 'editar_comercio.html', contexto)
             
-        # FLUJO EXCEPCIONAL 1: los datos ya existen
-        if nuevo_nombre == restaurante_actual.nombre:
-            messages.warning(request, "Datos ya existentes, por favor ingresa unos datos nuevos.")
-            return render(request, 'editar_comercio.html')
-        if nueva_contrasena == restaurante_actual.contrasena:
-            messages.warning(request, "Datos ya existentes, por favor ingresa unos datos nuevos.")
-            return render(request, 'editar_comercio.html')
+        # FLUJO ALTERNATIVO: los datos ya existen en otro restaurante
+        #se filtran los restaurantes de la base de datos y se excluye al restaurante actual
+        otros_restaurantes = Restaurante.objects.exclude(id=restaurante_actual.id)
+        
+        #se revisa si haya algun restaurante con la direccion nueva
+        if otros_restaurantes.filter(direccion=nueva_direccion).exists():
+            messages.warning(request, "La dirección ya está registrada en otro comercio. Por favor ingresa una diferente.")
+            return render(request, 'editar_comercio.html', contexto)
+        
+        #se revisa si haya algun restaurante con el telefono nuevo
+        if otros_restaurantes.filter(telefono=nuevo_telefono).exists():
+            messages.warning(request, "El teléfono ya está registrado en otro comercio. Por favor ingresa uno diferente.")
+            return render(request, 'editar_comercio.html', contexto)
+        
+        #se revisa si haya algun restaurante con la nueva contraseña
+        if otros_restaurantes.filter(contrasena=nueva_contrasena).exists():
+            messages.warning(request, "La contraseña ya está registrada en otro comercio. Por favor ingresa una diferente.")
+            return render(request, 'editar_comercio.html', contexto)
             
         # FLUJO NORMAL: Se conecta, guarda datos en la base de datos temporal
         if restaurante_actual:
