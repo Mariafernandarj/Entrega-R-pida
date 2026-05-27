fadd8e8f724b45656d9d022ffc8c1264af004d45
anasofiahdzz <anasofiahdzz@ciencias.unam.mx>
2026-05-27 13:18:10 -0600

    correcciones en busqueda de platillo

--- busqueda_restaurante_comida/urls.py
+++ busqueda_restaurante_comida/urls.py
@@ -1,7 +1,8 @@
+from navegar_menus.models import Platillo
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('resultados/', views.buscar_comida, name='buscar_comida'),
+    path('busqueda_platillo/', views.buscar_platillo, name='busqueda_platillo'),
     path('restaurante/<int:id>/', views.restaurante_seleccionado, name='restaurante_seleccionado'),
 ]
--- busqueda_restaurante_comida/views.py
+++ busqueda_restaurante_comida/views.py
@@ -1,35 +1,28 @@
 from django.shortcuts import render
-from django.contrib import messages
-# from core.models import Restaurante, Platillo  (para los modelos)
+from navegar_menus.models import Restaurante, Platillo
 
-def buscar_comida(request):
-    texto_busqueda = request.GET.get('q', '')
+def buscar_platillo(request):
+    # capta y revisa lo que el usuario esceribió en el buscador
+    texto_busqueda = request.GET.get('q', '').strip()
     
-    resultados_restaurantes = []
     resultados_platillos = []
 
     if texto_busqueda:
-        try:
-            # FLUJO NORMAL: Conecta a la BD y busca coincidencias
-            # resultados_restaurantes = Restaurante.objects.filter(nombre__icontains=texto_busqueda)
-            # resultados_platillos = Platillo.objects.filter(nombre__icontains=texto_busqueda)
+        # Busca en la base de datos 
+        platillos_encontrados = Platillo.objects.filter(nombre__icontains=texto_busqueda)
             
-            # FLUJO ALTERNATIVO: Si no hay resultados, manda un mensaje[cite: 2]
-            # if not resultados_restaurantes and not resultados_platillos:
-            #     messages.warning(request, f"No se encontraron resultados para '{texto_busqueda}'.")
-            pass 
-            
-        except Exception as e:
-            # FLUJO EXCEPCIONAL: Falla de conexión con la BD
-            messages.error(request, "Mensaje con aviso de falla en el sistema. Intente más tarde.")
+        for platillo in platillos_encontrados:
+            if getattr(platillo, 'restaurante', None):
+                resultados_platillos.append({
+                    'restaurante': platillo.restaurante,
+                    'platillo': platillo
+            })
 
-    # Manda los resultados a la pantalla busqueda_platillo
     contexto = {
         'texto_busqueda': texto_busqueda,
-        'restaurantes': resultados_restaurantes,
-        'platillos': resultados_platillos,
+        'resultados': resultados_platillos,
     }
-    return render(request, 'busquedaPlatillo.html', contexto)
+    return render(request, 'busquedaPlatillo.html', context=contexto)
 
 def restaurante_seleccionado(request, id):
     return render(request, 'restauranteSeleccionado.html', {'restaurante_id': id})
