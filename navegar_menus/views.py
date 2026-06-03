a11bec27ee0c6718b5a17d087ade54e6e7426b24
GaboDios <diazpayne.gabriel@ciencias.unam.mx>
2026-06-03 00:09:25 -0600

    Se agrega el cierre de sesion para el cliente

--- navegar_menus/views.py
+++ navegar_menus/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import logout
 from .models import Restaurante, Platillo
 
 def pagina_principal(request):
+    if not request.user.is_authenticated:
+        return redirect('iniciar_sesion')
     restaurantes = Restaurante.objects.all()
     platillos = Platillo.objects.all()
     return render(request, 'navegar_menus/pagina_principal.html', {
@@ -14,3 +17,11 @@ def detalle_platillo(request, platillo_id):
     return render(request, 'navegar_menus/detalle_platillo.html', {
         'platillo': platillo,
     })
+
+def cerrar_sesion(request):
+    logout(request)
+    response = redirect('iniciar_sesion')
+    response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response['Pragma'] = 'no-cache'
+    response['Expires'] = '0'
+    return response
