f78d0c659026d265b15261cecf102c8a64ef9ea4
GaboDios <diazpayne.gabriel@ciencias.unam.mx>
2026-06-10 22:59:22 -0600

    Gestionar menú: agregar, editar platillos y cambiar disponibilidad

--- principal_restaurante/urls.py
+++ principal_restaurante/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     # 👇 Se le añade el '/' al final del patrón de la URL
     path('principal_restaurante/', views.principal_restaurante, name='principal_restaurante'),
+    path('gestion_menu/', views.gestion_menu, name='gestion_menu'),
+    path('gestion_menu/agregar/', views.agregar_platillo, name='agregar_platillo'),
+    path('gestion_menu/editar/<int:platillo_id>/', views.editar_platillo, name='editar_platillo'),
+    path('gestion_menu/disponibilidad/<int:platillo_id>/', views.cambiar_disponibilidad, name='cambiar_disponibilidad'),
 ]
