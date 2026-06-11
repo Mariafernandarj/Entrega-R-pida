3f236adebb3a0ba5b51aab02736cb417bd21a7dc
Mariafernandarj <mafer_rj@ciencias.unam.mx>
2026-06-11 03:52:32 -0600

    Historial repartidor funciona bien

--- principal_restaurante/urls.py
+++ principal_restaurante/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    # 👇 Se le añade el '/' al final del patrón de la URL
     path('principal_restaurante/', views.principal_restaurante, name='principal_restaurante'),
 ]
